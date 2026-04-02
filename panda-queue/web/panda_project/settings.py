914f3c8bb23a37228e9f82e1e0f0f2f85316cab3
smk37git <sdmain3@gmail.com>
2026-04-02 16:19:06 -0500

    Updated the web settings.py

--- panda-queue/web/panda_project/settings.py
+++ panda-queue/web/panda_project/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv(Path(__file__).resolve().parent / '.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
