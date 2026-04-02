63bf641ba0ff8e23eb83bb4f26d0a7737884f304
smk37git <sdmain3@gmail.com>
2026-04-02 16:43:20 -0500

    Added webdev structure

--- panda-queue/web/queue_monitor/models.py
+++ panda-queue/web/queue_monitor/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
-# Create your models here.
+ 
+class QueueReading(models.Model):
+    timestamp = models.DateTimeField()
+    people_count = models.IntegerField()
+    estimated_wait_seconds = models.FloatField()
+    service_rate = models.FloatField(default=50.0)
+ 
+    class Meta:
+        ordering = ['-timestamp']
+        indexes = [models.Index(fields=['-timestamp'])]
+ 
+    def __str__(self):
+        return (f'{self.timestamp:%H:%M}: '
+                f'{self.people_count} people, '
+                f'{self.estimated_wait_seconds:.0f}s')
